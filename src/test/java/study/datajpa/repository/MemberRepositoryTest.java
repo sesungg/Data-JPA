76d308e843ec04511234c619ba662f5b6716ca86
sesung <sesungg@gmail.com>
2024-07-30 01:33:25 +0900

    확장 기능 - 사용자 저으이 리포지토리 구현

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -304,4 +304,10 @@ class MemberRepositoryTest {
         //when
         List<Member> result = memberRepository.findLockByUsername("member1");
     }
+
+    @Test
+    public void callCustom() throws Exception {
+        List<Member> memberCustom = memberRepository.findMemberCustom();
+
+    }
 }
